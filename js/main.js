51f253f8061c4bfabfb6ca0790bbf55f7f6ad3a0
Yuri <voltermaher@gmail.com>
2019-09-26 05:07:29 +0700

    opt-start

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
-$('.main-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  arrows : false
-});
+$(function() {
+  $('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows : false
+  });
 
-$('.slider-review').slick({
-  dots: false,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  arrows : true,
-  prevArrow: '.arrow-slider-right',
-  nextArrow:'.arrow-slider-left',
-});
+  $('.slider-review').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows : true,
+    prevArrow: '.arrow-slider-right',
+    nextArrow:'.arrow-slider-left',
+  });
 
-$('.slider-trust').slick({
-  dots: false,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 4,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  arrows : true,
-  prevArrow: '.arrow-slider-right',
-  nextArrow:'.arrow-slider-left',
+  $('.slider-trust').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows : true,
+    prevArrow: '.arrow-slider-right',
+    nextArrow:'.arrow-slider-left',
+  });
+
+  $('.basket__steps_step').each(function(i, item) {
+    console.log(item);
+    
+  });
+
 });
